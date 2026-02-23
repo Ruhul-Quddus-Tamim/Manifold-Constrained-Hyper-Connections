c1fc0cbc78788c0c025a9dbbfeab1c489a7517f8
Tamim <86849949+Ruhul-Quddus-Tamim@users.noreply.github.com>
2026-02-23 17:56:08 +0800

    Updated __init__.py

--- mhc/__init__.py
+++ mhc/__init__.py
@@ -2,25 +2,37 @@
 
 from mhc.data import (
     CharTokenizer,
+    CharLMTokenizer,
+    CharLMDataset,
     CopyDataset,
     HuggingFaceCopyDataset,
     create_dataloaders,
     create_dataloaders_from_hf,
+    create_lm_dataloaders,
+    create_lm_dataloader_full,
     load_hf_dataset,
 )
 from mhc.models import HyperTransformer, build_model_config
-from mhc.train_utils import autoregressive_generate, evaluate, train
+from mhc.models_gimhc import GIHCModule, GIHyperTransformer, build_gimhc_config
+from mhc.train_utils import autoregressive_generate, get_deepseek_step_scheduler, train
 
 __all__ = [
     "CharTokenizer",
+    "CharLMTokenizer",
+    "CharLMDataset",
     "CopyDataset",
     "HuggingFaceCopyDataset",
     "create_dataloaders",
     "create_dataloaders_from_hf",
+    "create_lm_dataloaders",
+    "create_lm_dataloader_full",
     "load_hf_dataset",
     "HyperTransformer",
     "build_model_config",
+    "GIHCModule",
+    "GIHyperTransformer",
+    "build_gimhc_config",
     "train",
-    "evaluate",
     "autoregressive_generate",
+    "get_deepseek_step_scheduler",
 ]
